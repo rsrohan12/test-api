8bf082cf08a824fe890cc8d151cab5965de28e79
Rohan <rsrohan787@gmail.com>
2024-10-12 13:50:11 +0530

    updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,6 +15,7 @@ export const signup = async(req, res) => {
         const hashPassword = await brcyptjs.hash(password, 10)
 
         const newUser = new User({
+            id: _id,
             name: name,
             email: email,
             password: hashPassword
@@ -59,6 +60,43 @@ export const login = async(req, res) => {
         console.log("Eror: " + error.message)
         res.status(500).json({message: "Internal server error"})
     }
-
     
+}
+
+export const dataUpdate = async(req, res) => {
+    try {
+        const {id} = req.params
+        const{name, email, password} = req.body
+
+        const user = await User.findById(id)
+
+        if (user) {
+            user.name = name || user.name // Keep the old name if no new one is provided
+            user.email = email || user.email // Keep the old email if no new one is provided
+            if(password){
+                const hashPassword = await brcyptjs.hash(password, 10)
+                user.password = hashPassword
+            }
+             // Keep the old password if no new one is provided
+            
+            const updatedUser = await user.save()
+
+            return res.status(201).json({
+                message: "User updated successfully",
+                user: {
+                    _id: updatedUser._id,
+                    name: updatedUser.name,
+                    email: updatedUser.email,
+                }
+            })
+        } else {
+            return res.status(400).json({message: "User does not exist"})
+        }
+
+        
+    } catch (error) {
+        console.log("Eror: " + error.message)
+        res.status(500).json({message: "Internal server error"})
+    }
+
 }
